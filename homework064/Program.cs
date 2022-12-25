cee658e3955facfc99b1467dc866f7d912e31448
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-26 00:46:39 +0300

    064

--- homework064/Program.cs
+++ homework064/Program.cs
@@ -5,3 +5,32 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+int GetNumber(string message)
+{
+   int result = 0;
+   while (true)
+   {
+      Console.WriteLine(message);
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         break;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число");
+      }
+   }
+   return result;
+}
+
+void GetValues(int n)
+{
+   Console.Write($"{n} ");
+   if (n > 1)
+   {
+      GetValues(n - 1);
+   }
+}
+
+int n = GetNumber("Введите число больше 1");
+GetValues(n);
--- homework066/Program.cs
+++ homework066/Program.cs
@@ -35,4 +35,3 @@ int Summ(int m, int n)
 int m = GetNumber("Введите число m");
 int n = GetNumber("Введите число n");
 Console.WriteLine($"{Summ(m, n)}");
-

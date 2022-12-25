05dad4d4eb163d2311437a93cd22c1bd164f3cd9
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-26 00:28:16 +0300

    068

--- homework068/Program.cs
+++ homework068/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью 
+рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int GetNumber(string message)
+{
+   int result = 0;
+   while (true)
+   {
+      Console.WriteLine(message);
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         break;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число");
+      }
+   }
+   return result;
+}
+
+int Ackerman(int n, int m)
+{
+   if (n == 0) return m + 1;
+   if (n != 0 && m == 0) return Ackerman(n - 1, 1);
+   if (n > 0 && m > 0) return Ackerman(n - 1, Ackerman(n, m - 1));
+   return Ackerman(n, m);
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+Console.WriteLine($"{Ackerman(m, n)}");

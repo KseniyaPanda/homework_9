b448fcba6916068c530e55bb3f36efdd8a98c358
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-26 00:38:53 +0300

    068

--- homework066/Program.cs
+++ homework066/Program.cs
@@ -4,3 +4,34 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+int GetNumber(string message)
+{
+   int result = 0;
+   while (true)
+   {
+      Console.WriteLine(message);
+      if (int.TryParse(Console.ReadLine(), out result))
+      {
+         break;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число");
+      }
+   }
+   return result;
+}
+
+int Summ(int m, int n)
+{
+   int summ = 0;
+   if (m <= n)
+   {
+      summ = m + Summ(m + 1, n);
+   }
+   return summ;
+}
+
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+Console.WriteLine($"{Summ(m, n)}");
